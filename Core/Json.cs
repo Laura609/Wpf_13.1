67c4380716ddde5323d53ffb8ee726ab5dbe6da7
Laura <lavrusik515@gmail.com>
2023-12-27 13:16:38 +0300

    Добавьте файлы проекта.

---
+++ Core/Json.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace Wpf_13._1.Core
+{
+    public class Json
+    {
+        public string Data1 { get; set; }
+        public int Data2 { get; set; }
+    }
+
+    public class AppDataService
+    {
+        private readonly string _filePath;
+
+        public AppDataService(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void Save(Json data)
+        {
+            var json = JsonSerializer.Serialize(data);
+            File.WriteAllText(_filePath, json);
+        }
+
+        public Json Load()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return null;
+            }
+
+            var json = File.ReadAllText(_filePath);
+            return JsonSerializer.Deserialize<Json>(json);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Text.Json;
+using Wpf_13._1.Core;
+
+namespace Wpf_13._1
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void AppDate_Click(object sender, RoutedEventArgs e)
+        {
+
+            var appDataService = new AppDataService("appData.json");
+
+            // Сохранение данных
+            var data = new Json
+            {
+                Data1 = "Hello, World!",
+                Data2 = 42
+            };
+            appDataService.Save(data);
+
+            // Загрузка данных
+            var loadedData = appDataService.Load();
+
+            MessageBox.Show($"{data}");
+        }
+    }
+}
